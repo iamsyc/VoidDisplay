365db5e1b1921e1af0f2f9473afd8ce21d67c24c
Phineas Guo <31276761+guoPhineas@users.noreply.github.com>
2025-11-28 20:41:35 +0800

    feat: add resolution options

---
+++ FreelyDisplay/Resolution/Resolution.swift
@@ -0,0 +1,19 @@
+//
+//  Resolution.swift
+//  FreelyDisplay
+//
+//  Created by Phineas Guo on 2025/11/28.
+//
+
+import Foundation
+
+extension Resolutions{
+    var id:UUID{UUID()}
+    var resolutions: (Int,Int){
+        let temp=self.rawValue.split(separator: "_")
+        let (width,height)=(temp[1],temp[2])
+        let resolution = (Int(width)!,Int(height)!)
+        return resolution
+    }
+    
+}
